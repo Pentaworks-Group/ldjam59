e006bf8490886fa04943739d5aa74bd4804c54e7
Rentem <salasam@gmx.net>
2026-04-20 10:15:58 +0200

    Popups for main menu

--- Code/ldjam59/Assets/Scripts/Scenes/MainMenu/MainMenuBehaviour.cs
+++ Code/ldjam59/Assets/Scripts/Scenes/MainMenu/MainMenuBehaviour.cs
@@ -9,26 +9,41 @@ namespace Assets.Scripts.Scenes.MainMenu
     public class MainMenuBehaviour : GameFrame.Core.UI.Scripts.Menu.BaseMenuBehaviour
     {
         [SerializeField] AudioEngine audioEngine;
+        [SerializeField] private GameObject OptionsPopup;
+        [SerializeField] private GameObject SavedGamesPopup;
+        [SerializeField] private GameObject GameModesPopup;
+
 
         private void Awake()
         {
-            string[] combinations = {"MenuMusic_1", "MenuMusic_2", "MenuMusic_3", "MenuMusic_3" };
+            string[] combinations = { "MenuMusic_1", "MenuMusic_2", "MenuMusic_3", "MenuMusic_3" };
             audioEngine.StartRandomCycling(combinations, 1, 7, 0.2f);
             audioEngine.Play();
         }
 
-        public void OnOpenOptions() => ChangeToScene(Constants.Scenes.Options);
-        public void OnOpenCredits() => ChangeToScene(Constants.Scenes.Credits);
-        public void OnOpenSavedGames() => ChangeToScene(Constants.Scenes.SavedGames);
-        public void OnOpenGameMode() => ChangeToScene(Constants.Scenes.GameMode);
+        public void OnOpenOptions()
+        {
+            OptionsPopup.SetActive(true);
+        }
+        public void OnOpenSavedGames()
+        {
+            SavedGamesPopup.SetActive(true);
+        }
+        public void OnOpenGameMode()
+        {
+            GameModesPopup.SetActive(true);
+        }
         public void OnQuit()
         {
             Base.Core.Game.Quit();
         }
 
-        public void TESTT()
+        public void CloserPopus()
         {
-            Debug.Log("TEEEEEEEEEEEEEEEEEEEST");
+            OptionsPopup.SetActive(false);
+            SavedGamesPopup.SetActive(false);
+            GameModesPopup.SetActive(false);
+            Debug.Log("Closing popups");
         }
     }
 }
