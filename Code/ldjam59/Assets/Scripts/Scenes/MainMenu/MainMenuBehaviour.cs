c9470f3a9667841d08286d8ae34430aa9211350d
Rentem <salasam@gmx.net>
2026-04-20 13:52:31 +0200

    mainMenu esc & removed unused imports

--- Code/ldjam59/Assets/Scripts/Scenes/MainMenu/MainMenuBehaviour.cs
+++ Code/ldjam59/Assets/Scripts/Scenes/MainMenu/MainMenuBehaviour.cs
@@ -1,8 +1,5 @@
-﻿using System;
-
-using GameFrame.Core;
-
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Assets.Scripts.Scenes.MainMenu
 {
@@ -13,14 +10,26 @@ namespace Assets.Scripts.Scenes.MainMenu
         [SerializeField] private GameObject SavedGamesPopup;
         [SerializeField] private GameObject GameModesPopup;
 
+        private InputAction escapeAction;
 
         private void Awake()
         {
             string[] combinations = { "MenuMusic_1", "MenuMusic_2", "MenuMusic_3", "MenuMusic_3" };
             audioEngine.StartRandomCycling(combinations, 1, 7, 0.2f);
             audioEngine.Play();
+            escapeAction = InputSystem.actions.FindAction("Escape");
+            escapeAction.performed += OnEscapePressed;
         }
 
+        private void OnDisable()
+        {
+            escapeAction.performed -= OnEscapePressed;
+        }
+
+        private void OnEscapePressed(InputAction.CallbackContext context)
+        {
+            CloserPopus();
+        }
         public void OnOpenOptions()
         {
             OptionsPopup.SetActive(true);
