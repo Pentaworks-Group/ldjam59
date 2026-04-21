c5db318278f1b0b6aec75e32b97a5f328e87afd2
XpArKeR <xparker@gmx.net>
2026-04-21 02:12:41 +0200

    VersionText

--- Code/ldjam59/Assets/Scripts/Scenes/Start/StartBehaviour.cs
+++ Code/ldjam59/Assets/Scripts/Scenes/Start/StartBehaviour.cs
@@ -1,12 +1,18 @@
+using TMPro;
+
 using UnityEngine;
 
 namespace Assets.Scripts.Scenes.Start
 {
     public class StartBehaviour : MonoBehaviour
     {
+        [SerializeField] private TMP_Text versionText;
+
         private void Awake()
         {
             _ = Base.Audio.AudioEngine;
+
+            versionText.text = $"{Application.version}";
         }
 
         public void OnClick()
